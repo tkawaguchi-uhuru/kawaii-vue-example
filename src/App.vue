cb94c3c96d90e0794c25b39e90268936f51aedee
tkawag <taketomo.kawaguchi@uhuru.jp>
2019-11-27 16:31:10 +0900

    画面遷移を追加

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
+      <router-link to="/menu">Menu</router-link> |
+      <router-link to="/search">Search</router-link> |
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
