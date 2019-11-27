dc1471e96bfb041d4380506a3af76b91a2b92725
tkawag <taketomo.kawaguchi@uhuru.jp>
2019-11-27 16:58:09 +0900

    サブコンポーネントにCSSを追加

--- src/components/ResultList.vue
+++ src/components/ResultList.vue
@@ -11,5 +11,31 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.p-list {
+  margin: 0 16px 40px;
+  &__item {
+    width: 100%;
+    background: #fff;
+    border-radius: 4px;
+    filter: drop-shadow(0px 1px 3px rgba(0, 0, 0, 0.2));
+    margin: 0 0 16px;
+    padding: 16px;
+    text-align: left;
+    color: rgba(0, 0, 0, 0.87);
+
+    .p-title {
+      font-size: 24px;
+      margin: 8px 0;
+    }
+
+    .p-text {
+      font-size: 14px;
+      letter-spacing: 0.02em;
+      line-height: 20px;
+      color: rgba(0, 0, 0, 0.6);
+      margin: 0;
+    }
+  }
+}
 </style>
--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -11,5 +11,50 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.p-search-box {
+  background: #f1f1f1;
+  border-radius: 4px;
+  display: inline-block;
+  position: relative;
+  margin: 0 0 24px;
+
+  input {
+    width: 288px;
+    height: 56px;
+    border-radius: 4px;
+    padding: 0 48px 0 16px;
+    letter-spacing: 0.01em;
+    color: rgba(0, 0, 0, 0.87);
+  }
+
+  &__icon {
+    position: absolute;
+    top: 50%;
+    right: 0;
+    transform: translateY(-50%);
+    padding: 12px;
+    cursor: pointer;
+  }
+
+  &::after {
+    content: '';
+    width: 100%;
+    height: 1px;
+    display: block;
+    background: transparent;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+  }
+
+  &:hover {
+    background: #ebebeb;
+    border-radius: 4px 4px 0 0;
+
+    &::after {
+      background: #727272;
+    }
+  }
+}
 </style>
