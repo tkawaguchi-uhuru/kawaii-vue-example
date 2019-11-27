4bf148b51d3e48173580d0c71745daff5588359c
tkawag <taketomo.kawaguchi@uhuru.jp>
2019-11-27 17:12:34 +0900

    結果リストにダミーデータを設置して画面にバインド

--- src/components/ResultList.vue
+++ src/components/ResultList.vue
@@ -1,12 +1,26 @@
 <template>
-  <div>結果リスト</div>
+  <div>
+    <ul class="p-list">
+      <li v-for="item in dummydata" :key="item.id" class="p-list__item">
+        <img :src="item.img" :alt="item.title">
+        <h3 class="p-title">{{item.title}}</h3>
+        <p class="p-text">{{item.body}}</p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'ResultList',
   data () {
-    return {}
+    return {
+      dummydata: [
+        { id: 0, title: 'データ1', body: 'データ1の内容です。123', img: '' },
+        { id: 1, title: 'データ2', body: 'データ2の内容です。456', img: '' },
+        { id: 2, title: 'データ3', body: 'データ3の内容です。789', img: '' }
+      ]
+    }
   }
 }
 </script>
